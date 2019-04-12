613594b0b5ed3e0be353a39e589fdf93bfc4e001
Wesley Ribeiro <wes.rsilva@gmail.com>
2019-04-12 17:57:45 -0300

    Instalar e configurar pacote CORS para habilitar acesso externo

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 const routes = require('./src/controller/routes');
 
 const app = express();
 
 // Permite que envie dados para aplicação em formato JSON através do BODY da request:
 app.use(express.json());
+// Habilitando CORS para todos os domínios possíveis
+app.use(cors());
 app.use('/api', routes);
 
 app.listen(3000);
