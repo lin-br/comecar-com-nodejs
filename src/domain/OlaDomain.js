78889d596ad1fd869d44b8890d39020067c36037
Wesley Ribeiro <wes.rsilva@gmail.com>
2019-04-12 17:44:10 -0300

    Configurar rota e leitura de parâmetros da requisição

--- src/domain/OlaDomain.js
+++ src/domain/OlaDomain.js
@@ -5,5 +5,9 @@ module.exports = {
     async store(request, response) {
         console.log(request.body);
         return response.status(201).send(request.body);
-    }
+    },
+    async show(request, response) {
+        console.log(request.params.id);
+        return response.status(200).send(request.params.id);
+    },
 };
